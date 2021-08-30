03910fb5683658bc3760dee9be63431246f03b8b
Christy <87586972+Christy915@users.noreply.github.com>
2021-08-30 15:58:51 +1200

    Add files via upload
    modified

--- hangman2.0.rb
+++ hangman2.0.rb
@@ -1,9 +1,34 @@
-print "Insert a letter:"
+word_box = ["Christy", "Ruby", "wolf", "ball", "lamby", "English"]
+
+word = word_box.sample.downcase
+result = []
+letter_box = []
+print "The word you are guessing has #{word.length} letters."
+
+loop do
+print "Now insert a letter:"
 letter_inserted = gets.chomp.downcase
 
-letter_box = []
-letter_box.push(letter_inserted)
-letter_box.each do
-  puts "The letters you have inserted are #{letter_box}."
+if letter_box.include? letter_inserted
+  letter_box.uniq
+  puts "You have tried this letter before. Try another one!"
+  next
 end
- 
+letter_box.push(letter_inserted)
+puts "The letter(s) you have inserted is(are) #{letter_box}."
+
+if word.include? letter_inserted
+  puts "You got it!"
+  result.push(letter_inserted)
+  dif = word.split(//).difference(result)
+  if dif.length > 0
+    puts "You have only #{dif.length} letters left."
+  else
+    puts "Success! The answer is #{word}!"
+  end
+else
+  puts "What about trying another letter?"
+end
+
+break  if word.split(//).sort == result.sort
+end
